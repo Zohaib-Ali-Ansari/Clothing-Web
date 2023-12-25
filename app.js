3e7724fe479bc1f8b275dadc20f50cbc7922cef5
Zohaib-Ali-Ansari <zohaibaliansari5@gmail.com>
2023-12-25 12:08:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-let button = document.getElementsByClassName("more-prd-btn")[0];
-let box5 = document.getElementsByClassName("box-5")[0];
-let box6 = document.getElementsByClassName("box-6")[0];
-let box7 = document.getElementsByClassName("box-7")[0];
-let box8 = document.getElementsByClassName("box-8")[0];
-let offerContainer = document.getElementsByClassName("offer-container")[0];
-let mainContainer = document.getElementsByClassName("main-container")[0];
-let footer = document.getElementsByClassName("footer")[0];
-let contactHeading = document.getElementById("contact-heading");
-
-
 let currentSlide = 0;
 let slides = document.querySelectorAll('.slide');
 
